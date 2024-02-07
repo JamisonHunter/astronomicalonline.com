5d58e4ec479da15d57de2f7c225b766c6d21fe35
JamisonHunter <126278717+JamisonHunter@users.noreply.github.com>
2024-02-07 02:57:51 +0000

    Added Additional Images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+# app.py
 from flask import Flask, render_template
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import datetime, timedelta
 import creds
 
 app = Flask(__name__, static_folder='static')
@@ -19,7 +20,18 @@ def index():
   # Query MongoDB for the document with today's date
   today_image = collection.find_one({"Date": today})
 
-  return render_template("index.html", image=today_image)
+  # Get the images for the previous six days
+  six_days_ago = (datetime.today() - timedelta(days=6)).strftime('%Y-%m-%d')
+  previous_images = collection.find({
+      "Date": {
+          "$gte": six_days_ago,
+          "$lt": today
+      }
+  }).sort("Date", -1).limit(6)
+
+  return render_template("index.html",
+                         image=today_image,
+                         previous_images=previous_images)
 
 
 if __name__ == "__main__":
